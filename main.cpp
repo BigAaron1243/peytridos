1bac18b94732f74de2221e88bebba93b400d5c87
vexere <bigaaron1243@gmail.com>
2022-05-21 14:11:08 +1000

    added phyiscs and basic objects, physics needs optimisation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <stdio.h>
+#include <math.h>
 
 struct simpleobject {
     float r;
@@ -91,12 +92,17 @@ int main()
 
     end = get_end(head);
     for (int i = 0; i < 1000; i++) {
-        end = push_end(end, add_simpleobject(5,rand() % 700, rand() % 700,0,0,0,0));
+      end = push_end(end, add_simpleobject(5,rand() % 700, rand() % 700,0,0,0,0));
     }
 
+//        end = push_end(end, add_simpleobject(15, 350, 360,0,0.1,0,0));
+  //      end = push_end(end, add_simpleobject(20, 600, 350,0,-0.2,0,0));
+    //    end = push_end(end, add_simpleobject(30, 410, 350,0,0,0,0));
+      //  end = push_end(end, add_simpleobject(5, 570, 350,0,0,0,0));
 
     while (window.isOpen())
     {
+ //       end = push_end(end, add_simpleobject(5, 150, 160,0,0,0,0));
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -106,14 +112,60 @@ int main()
             }
         }
 
-        //Draw step
+        //Physics step
+        node_t * i_obj = head;
+
+        for (node_t * i_obj = head; i_obj; i_obj = i_obj->next)
+        {
+            for (node_t * j_obj = head; j_obj; j_obj = j_obj->next)
+            {
+                sf::Vector2f col_vec;
+                sf::Vector2f col_vec_norm;
+                float psum_r = pow(i_obj->data->r + j_obj->data->r, 2);
+                float pdist = pow(i_obj->data->x - j_obj->data->x, 2) + pow(i_obj->data->y - j_obj->data->y, 2);
+                if (i_obj != j_obj) {
+              //  printf("psum:%g, pdist:%g, ir:%g, jr:%g, ix:%g, jx:%g, iy:%g, jy:%g, ivx:%g, jvx:%g\n ", psum_r, pdist,  i_obj->data->r,  j_obj->data->r, i_obj->data->x, j_obj->data->x, i_obj->data->y, j_obj->data->y, i_obj->data->vx, j_obj->data->vx);
+                }
+                if((i_obj != j_obj) && (pdist < psum_r)) {
+                    col_vec = {i_obj->data->x - j_obj->data->x, i_obj->data->y - j_obj->data->y};
+                    float norm_mod = sqrt(pow(col_vec.x, 2) + pow(col_vec.y, 2));
+                    col_vec_norm = {col_vec.x / norm_mod, col_vec.y / norm_mod};
+                    if (pdist == 0)
+                    {
+                        i_obj->data->x += (((rand() % 2) * 2) - 1) * 5;
+                        i_obj->data->y += (((rand() % 2) * 2) - 1) * 5;
+                    } 
+                    else
+                    {
+                        float dsp = 0.02;
+                        i_obj->data->x += col_vec.x * dsp;
+                        i_obj->data->y += col_vec.y * dsp;
+                        j_obj->data->x -= col_vec.x * dsp;
+                        j_obj->data->y -= col_vec.y * dsp;
+
+                        
+                        i_obj->data->vx += fabs(j_obj->data->vx) * col_vec_norm.x / 2;
+                        i_obj->data->vy += fabs(j_obj->data->vx) * col_vec_norm.y / 2;
+                        j_obj->data->vx -= fabs(i_obj->data->vx) * col_vec_norm.x / 2;
+                        j_obj->data->vy -= fabs(i_obj->data->vx) * col_vec_norm.y / 2;
+                    }         
+                }
+            }
+        }
+
+        //Draw step (i know its stupid but bear with me here, im doing my best
         window.clear();
         node_t * current = head;
-        while (current->next != NULL) {
-            sf::CircleShape tempshape(4);
+        while (current != NULL)
+        {
+            current->data->x += current->data->vx;
+            current->data->y += current->data->vy;
+            current->data->w += current->data->vw;
+
+            sf::CircleShape tempshape(current->data->r);
             tempshape.setPosition(current->data->x, current->data->y);
-            window.draw(tempshape);
             current = current->next;
+            window.draw(tempshape);
         }
         window.display();
     }
